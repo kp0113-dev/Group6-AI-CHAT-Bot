7f59b1ebad4bfd23dcf63c7dbbe9990bca4d6507
Kyler772 <157929096+Kyler772@users.noreply.github.com>
2025-09-04 13:33:28 -0500

    Update lambda_test.py

--- lambdas/lambda_test.py
+++ lambdas/lambda_test.py
@@ -1,5 +1,24 @@
-def lambda_test(event, context):
-    return {
-        'statusCode': 200,
-        'body': 'Hello-Kyler!'
+import java 
+def lambda_handler(event, context):
+    #Log what LexBot sent to Lambda
+    print("Recieved event from Lexbot")
+    print("Recieved text from Lexbot")
+    print(json.dumps event)
+    return java.lang.System.getProperty("java.version")
+    return "Hello from Lambda"
+
+#Used to access LexBot
+user_input = event.get("inputTrnascript", "No input provided")
+
+#Returns to lex with a simple message
+response = {
+    "dialogAction": {
+        "type": "Close",
+        "fulfillmentState": "Fulfilled",
+        "message": {
+            "contentType": "PlainText",
+            "content": "Hello from Lambda, input recieved: {user_input}"
+        }
     }
+}
+return response 
