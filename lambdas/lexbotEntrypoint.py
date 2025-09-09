3f224f8ad5a2056f8187e949a8e6823572527270
kp0113-dev <kp0113@uah.edu>
2025-09-09 18:09:23 -0500

    Call searchDynamoDB and respond with search result

--- lambdas/lexbotEntrypoint.py
+++ lambdas/lexbotEntrypoint.py
@@ -1,14 +1,43 @@
 import json
+import boto3
+
+lambda_client = boto3.client("lambda")
 
 def lambda_handler(event, context):
+    # Print statement for Debugging in CloudWatch
     print("Received event:", json.dumps(event))
 
     # Get the user input text
     user_input = event.get("inputTranscript", "")
+    
+    # Set default response
+    response_text = "Deafult Response"
 
-    # Get the current intent name
+    # Get the current intent items
     intent_name = event["sessionState"]["intent"]["name"]
+    slots = event["sessionState"]["intent"].get("slots", {})
+    interpreted_value = None
+    
+    if "location" in slots and slots["location"] and "value" in slots["location"]:
+        interpreted_value = slots["location"]["value"].get("interpretedValue")
 
+    if not interpreted_value:
+        response_text = f"Could not determine interpreted value for {intent_name}"
+    else:
+        # Call searchDynamoDB Lambda
+        payload = {
+            "intentName": intent_name,
+            "interpretedValue": interpreted_value
+        }
+        response = lambda_client.invoke(
+            FunctionName="searchDynamoDB-dev-kamil",
+            InvocationType="RequestResponse",
+            Payload=json.dumps(payload)
+        )
+        search_result = json.loads(response["Payload"].read())
+        response_text = search_result.get("result", "No result returned.")
+        
+    # MAIN RETURN RESPONSE
     return {
         "sessionState": {
             "dialogAction": {
@@ -22,7 +51,7 @@ def lambda_handler(event, context):
         "messages": [
             {
                 "contentType": "PlainText",
-                "content": f"You said: {user_input}"
+                "content": f"{response_text}"
             }
         ]
     }
