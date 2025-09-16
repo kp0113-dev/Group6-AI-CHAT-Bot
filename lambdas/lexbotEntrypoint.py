8f86c4abb22bf5ba4526b0d3396940717a185f0a
kp0113-dev <kp0113@uah.edu>
2025-09-16 10:41:05 -0500

    Add functionality for save slot response

--- lambdas/lexbotEntrypoint.py
+++ lambdas/lexbotEntrypoint.py
@@ -1,25 +1,30 @@
 import json
 import boto3
-from heuristics.heuristics import can_reuse_subject
+from lambdas.heuristics.heuristics import can_reuse_subject
+
 
 lambda_client = boto3.client("lambda")
+
 
 def lambda_handler(event, context):
     # Print statement for Debugging in CloudWatch
     print("Received event:", json.dumps(event))
 
+
     # Get the user input text
     user_input = event.get("inputTranscript", "")
-    
+   
     # Set default response
     response_text = "Deafult Response"
+
 
     # Get the current intent items
     intent_name = event["sessionState"]["intent"]["name"]
     slots = event["sessionState"]["intent"].get("slots", {})
+    session_attrs = event["sessionState"].get("sessionAttributes", {})
     resolved_value = None
-    
-    # Check if inetent is fulfilled and check if slot has 'value'
+   
+    # Check if intent is fulfilled and check if slot has 'value'
     # If slot value exists then set the resolved value
     # If slot value exists but resolved value does not then default to interpreted value
     for slot_name, slot_data in slots.items():
@@ -27,20 +32,26 @@ def lambda_handler(event, context):
             resolved_value_list = slot_data["value"].get("resolvedValues", [])
             try:
                 resolved_value = resolved_value_list[0] # resolved_value returns as a list so pull the value from the first index
+                session_attrs["savedResolvedValue"] = resolved_value # update saved subject value
             except IndexError:
                 resolved_value = slots["location"]["value"].get("interpretedValue")
             print(f"Resolved value for {slot_name}: {resolved_value}")
 
+
     # Return back to Lex and invoke slot prompt asking user to specify slot value
     if resolved_value is None:
-        return {
-            "sessionState": {
-                "dialogAction": {"type": "Delegate"},
-                "intent": event["sessionState"]["intent"]
+        if can_reuse_subject(intent_name) and "savedResolvedValue" in session_attrs:
+            resolved_value = session_attrs["savedResolvedValue"]
+        else:
+            return {
+                "sessionState": {
+                    "dialogAction": {"type": "Delegate"},
+                    "intent": event["sessionState"]["intent"]
+                }
             }
-        }
 
-    else:
+
+    if resolved_value is not None:
         # Call searchDynamoDB Lambda
         payload = {
             "intentName": intent_name,
@@ -53,10 +64,11 @@ def lambda_handler(event, context):
         )
         search_result = json.loads(response["Payload"].read())
         response_text = search_result.get("result", "No result returned.")
-        
+       
     # MAIN RETURN RESPONSE
     return {
         "sessionState": {
+            "sessionAttributes": session_attrs,
             "dialogAction": {
                 "type": "Close"   # End the conversation
             },
